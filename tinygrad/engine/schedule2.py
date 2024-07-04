b603f0b07e6c3a9f97c95d866f0591f53bd6ba89
qazal <qazal.software@gmail.com>
2024-07-04 14:31:55 +0300

    milestone: test_ops passes

--- tinygrad/engine/schedule2.py
+++ tinygrad/engine/schedule2.py
@@ -12,7 +12,7 @@ from tinygrad.shape.symbolic import Variable
 
 def lower_lazybuffer(out:LazyBuffer, global_stores:Dict[LazyBuffer, None]) -> Tuple[LazyOp, List[LazyBuffer]]:
   if out.op in {LoadOps.CUSTOM, LoadOps.COPY, LoadOps.EMPTY, LoadOps.VIEW}: return LazyOp(out.op, (), out.arg), list(out.srcs)
-  inputs: List[LazyBuffer] = []
+  inputs: Dict[LazyBuffer, int] = {}
   @functools.lru_cache(None)
   def _dfs(x:LazyBuffer, st:ShapeTracker):
     if x != x.base:
@@ -20,16 +20,14 @@ def lower_lazybuffer(out:LazyBuffer, global_stores:Dict[LazyBuffer, None]) -> Tu
       x = x.base
     if x.op is LoadOps.CONST: return LazyOp(BufferOps.CONST, (), ConstBuffer(x.arg, x.dtype, st))
     if x is not out and (x in global_stores or x.realized is not None):
-      assert x not in inputs
-      inputs.append(x)
-      return LazyOp(BufferOps.LOAD, (), MemBuffer(len(inputs), x.dtype, st))
+      return LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.setdefault(x, len(inputs)+1), x.dtype, st))
     if x.op in ReduceOps:
       assert x is out, f"{x} != {out}"
       st = ShapeTracker.from_shape(x.srcs[0].shape)
     lop = _dfs(x.srcs[0], st) if x.op in {LoadOps.CONTIGUOUS, LoadOps.ASSIGN} else LazyOp(cast(Op, x.op), tuple(_dfs(s, st) for s in x.srcs), x.arg)
     if x is out: lop = LazyOp(BufferOps.STORE, (lop, ), MemBuffer(0, x.dtype, x.st))
     return lop
-  return _dfs(out, out.st), inputs
+  return _dfs(out, out.st), list(inputs)
 
 def create_schedule(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Variable, int]]:
   global_stores = {x.base:None for x in outs if x.base.op is not LoadOps.CONST and x.base.realized is None}
@@ -60,13 +58,11 @@ def create_schedule(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Var
     n = queue.pop(0)
     del n.srcs
     lop, inputs = rev_children[n]
-    if DEBUG >= 3:
-      print("---")
-      print_tree(lop)
     schedule.append(ScheduleItem((lop, ), (n.buffer, )+tuple(x.buffer for x in inputs)))
     for x in children[n]:
       in_degree[x] -= 1
       if in_degree[x] == 0: queue.append(x)
 
   assert len(schedule) == len(global_stores), f"{len(schedule)} != {len(global_stores)}"
+  if DEBUG >= 1 and len(schedule) >= 10: print(f"scheduled {len(schedule)} kernels")
   return schedule, {}
