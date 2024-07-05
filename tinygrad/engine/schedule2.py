ef85e8f808e9ac0ce079b90863f355b450b03964
qazal <qazal.software@gmail.com>
2024-07-05 12:09:32 +0300

    cleanups

--- tinygrad/engine/schedule2.py
+++ tinygrad/engine/schedule2.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass
-from enum import Enum, EnumType
+from enum import EnumType
 import functools
-import itertools
-from typing import Any, Callable, DefaultDict, Dict, List, Literal, Optional, Set, Tuple, TypeVar, Union, cast
+from typing import Callable, DefaultDict, Dict, List, Optional, Set, Tuple, cast
 
 from tinygrad.engine.graph import print_tree
 from tinygrad.engine.schedule import ScheduleItem
@@ -25,20 +24,11 @@ def lower_lazybuffer(out:LazyBuffer, global_stores:Dict[LazyBuffer, None]) -> Tu
       x = x.base
     if x.op is LoadOps.CONST: return LazyOp(BufferOps.CONST, (), ConstBuffer(x.arg, x.dtype, st))
     if out.op is LoadOps.ASSIGN and x is out.srcs[1]: return LazyOp(BufferOps.LOAD, (), MemBuffer(0, x.dtype, st))
-    if x is not out and (x in global_stores or x.realized is not None):
-      return LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.setdefault(x, len(inputs)+1), x.dtype, st))
-    if x.op in ReduceOps:
-      assert x is out, f"{x} != {out}"
-      output_st = x.st
-      st = ShapeTracker.from_shape(x.srcs[0].shape)
-    if x.op is LoadOps.ASSIGN:
-      assert x is out
-      assert x.srcs[1].base is x.srcs[1], "assign must be to base"
-      assert x.srcs[1].realized is not None, f"assign must be already realized to schedule {x.srcs[1]}"
-      if len(out.arg) != 0: output_st = cast(ShapeTracker, out.arg[0])
+    if x is not out and (x in global_stores or x.realized is not None): return LazyOp(BufferOps.LOAD, (), MemBuffer(inputs.setdefault(x, len(inputs)+1), x.dtype, st))
+    if x.op in ReduceOps: output_st, st = x.st, ShapeTracker.from_shape(x.srcs[0].shape)
+    if x.op is LoadOps.ASSIGN and len(out.arg) != 0: output_st = cast(ShapeTracker, out.arg[0])
     lop = _dfs(x.srcs[0], st) if x.op in {LoadOps.CONTIGUOUS, LoadOps.ASSIGN} else LazyOp(cast(Op, x.op), tuple(_dfs(s, st) for s in x.srcs), x.arg)
-    if x is out: lop = LazyOp(BufferOps.STORE, (lop, ), MemBuffer(0, x.dtype, output_st))
-    return lop
+    return LazyOp(BufferOps.STORE, (lop, ), MemBuffer(0, x.dtype, output_st)) if x is out else lop
   return _dfs(out, output_st:=out.st), list(inputs)
 
 @dataclass(frozen=True)
@@ -51,28 +41,20 @@ class OpPat:
 class PatternMatcher:
   def __init__(self, patterns:List[Tuple[OpPat, Callable]], children:DefaultDict[LazyBuffer, Dict[LazyBuffer, None]], rev_children:Dict[LazyBuffer, Tuple[LazyOp, List[LazyBuffer]]]):
     self.patterns, self.children, self.rev_children = patterns, children, rev_children
-  def rewrite(self, op:LazyBuffer) -> Optional[LazyBuffer]:
-    for p,fxn in self.patterns:
-      store: Dict[str, LazyBuffer] = {}
-      if _match(op, p, store) and (ret:=fxn(**store)) is not None: return ret  # NOTE: if it returns None, we keep trying to match
-    return None
 
 def _fuse_elementwise_child(r:LazyBuffer, e:LazyBuffer, ctx:PatternMatcher):
   output_st = r.st
-  inputs = set(ctx.rev_children[r][1]).union(filter(lambda x: x is not r, ctx.rev_children[e][1]))
-  def _replace(x:LazyOp):
-    src = tuple(map(_replace, x.src))
-    print(src)
-
-  raise Exception(output_st)
+  r_ast, r_inputs = ctx.rev_children[r]
+  e_ast, e_inputs = ctx.rev_children[e]
+  print_tree(r_ast)
+  print_tree(e_ast)
+  replace_idxs = {}
+  raise Exception()
 
 elementwise_ops = {UnaryOps, BinaryOps, TernaryOps}
 kernel_fusion = [
     (OpPat(ReduceOps, "r", children=OpPat(elementwise_ops, "e"), ), lambda r,e,ctx: _fuse_elementwise_child(r, e, ctx)),
 ]
-
-def graph_rewrite(store:LazyOp, pm:PatternMatcher):
-  raise Exception(store)
 
 def create_schedule(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Variable, int]]:
   if DEBUG >= 3: print(colored(f"scheduling {outs}", "yellow"))
@@ -104,6 +86,7 @@ def create_schedule(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Var
         children[x][buf] = None
         in_degree[buf] += 1
 
+  """
   def _match(op:LazyBuffer, children:DefaultDict[LazyBuffer, Dict[LazyBuffer, None]], pat:OpPat, store:Dict[str, LazyBuffer]) -> bool:
     if pat.name is not None and store.setdefault(pat.name, op) is not op: return False
     if pat.op is not None and (not any(op.op in opset for opset in pat.op) if isinstance(pat.op, set) else op.op not in pat.op): return False
@@ -114,15 +97,13 @@ def create_schedule(outs:List[LazyBuffer]) -> Tuple[List[ScheduleItem], Dict[Var
       store = new_store
       return True
     return False
-
   ctx = PatternMatcher(kernel_fusion, children, rev_children)
-
   for out, chld in children.items():
     pattern = kernel_fusion[0]
     print(pattern, out)
     x = _match(out, children, pattern[0], store:={})
     if x: ret = pattern[1](**store, ctx=ctx)
-  print("------------")
+  """
 
   queue = [x for x in global_stores if in_degree[x] == 0]
   schedule: List[ScheduleItem] = []
